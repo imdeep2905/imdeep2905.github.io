cbc2de2a4c0e8c7742d765cc78a6b566af38cdba
unknown <deepraval2905@gmail.com>
2020-07-01 11:27:10 +0530

    testing push

--- CRMS/Models/1D/script.py
+++
@@ -1,7 +0,0 @@
-import os
-import tensorflow as tf
-import tensorflowjs as tfjs
-
-tf.compat.v1.disable_eager_execution()
-model=tf.keras.models.load_model('1D.h5')
-tfjs.converters.save_keras_model(model, '.')
--- CRMS/predict.js
+++ CRMS/predict.js
@@ -1,19 +1,30 @@
 $("#csv-selector").change(function ()
 {
+    console.log("csvselect");
     let reader = new FileReader();
-    reader.onload = function ()
-    {
-        ;
+    reader.onload = function () {
+        let dataURL = reader.result;
+        $("#prediction-list").empty();
     }
-    let file = $("#csv-selector").prop('files')[0];
+    let file = $("#csv-selector").prop("files")[0];
     reader.readAsDataURL(file);
+    console.log(file);
 });
 
 let model;
 
 (async function()
 {
-    $(".progress-bar").show(); 
-    model = await tf.loadLayersModel('Models/1D/model.json');
+    //model = await tf.loadLayersModel('Models/1D/model.json');
     $('.progress-bar').hide();
+    //console.log(model.summary());
 })();
+
+$("#predict-btn").click(async function () 
+{
+    console.log("called");
+    var new_csv = d3.csv.parseRows(file).slice(1).map(function (d) {
+        return { date: d[0], one: d[1], two: d[2], three: d[3] };
+    })
+    console.log(new_csv);
+});
